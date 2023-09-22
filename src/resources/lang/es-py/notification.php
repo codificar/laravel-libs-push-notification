cb833229bc7b4754b41aae152ad85b984f667bfa
Randler Ferraz Almeida <randler.ferraz@codificar.com.br>
2023-09-22 16:41:18 -0300

    correção tradução es-py

---
+++ src/resources/lang/es-py/notification.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    'user' => 'Usuario',
+    'provider' => 'Conductor',
+    'push_settings' => 'Configuración de Notificaciones Push',
+    'home' => 'Inicio',
+    'push_notification' => 'Notificaciones Push',
+    'ios_settings' => 'Configuraciones de iOS',
+    'android_settings' => 'Configuraciones de Android',
+    'team_id' => 'ID de Equipo',
+    'key_id' => 'ID de Clave',
+    'provider_package' => 'Paquete de la Aplicación del Proveedor',
+    'user_package' => 'Paquete de la Aplicación del Usuario',
+    'save_ios_settings' => 'Guardar Configuraciones de iOS',
+    'settings_saved' => 'Configuraciones Guardadas',
+    'yes' => 'Sí',
+    'no' => 'No',
+    'save_android_settings' => 'Guardar Configuraciones de Android',
+    'gcm_browser_key' => 'Clave de Navegación GCM',
+    'audio_push' => 'Notificación Push de Audio para Android',
+    'audio_push_cancellation' => 'Audio de Cancelación de Viaje',
+    "audio_url" => 'URL de Audio de Sonido de Viaje',
+    "audio_uploaded" => 'Archivo de Audio Subido'
+);
--- tests/translate/LibPushLanguageSpanishTest.php
+++ tests/translate/LibPushLanguageSpanishTest.php
@@ -19,7 +19,7 @@ class LibPushLanguageSpanishTest extends TestCase
         
         $path = base_path() . '/vendor/codificar/push-notification/src/resources/lang';
         $directoryPt = "$path/pt-br/*.php";
-        $directoryEs = "$path/es/*.php";
+        $directoryEs = "$path/es-py/*.php";
         $filesPt = glob($directoryPt);
         $filesEs = glob($directoryEs);
 
